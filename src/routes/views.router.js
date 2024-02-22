24af774a5ac1d4e800852aeb3bb99642405fa379
PMQZArg <pmqz.ar@gmail.com>
2024-02-21 21:57:25 -0300

    Desafio4BackEndPabloMarquez

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,11 +1,13 @@
 import express from "express";
+import { ProductManager } from "../manager/productManager";
 //creando un router
 const router = express.Router();
 
+const p = new ProductManager
 
 router.get("/", (req,res)=>{
     const products = p.getProducts();
-    res.render("index",{}) //por ahora solo se renderiza la vista, sin pasar objeto {}, despues ... vemos.
+    res.render("index",{products}) //por ahora solo se renderiza la vista, sin pasar objeto {}, despues ... vemos.
 })
 
 export default router;
